5466efd71b2f7be02e271e62188990f52cee8664
Yevhen Mekhedov <yevhen.mekhedov@customertimes.com>
2023-04-09 16:05:36 +0300

    First project commit

---
+++ TestXcodeCloud/TestXcodeCloudApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestXcodeCloudApp.swift
+//  TestXcodeCloud
+//
+//  Created by Yevhen Mekhedov on 09.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct TestXcodeCloudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
